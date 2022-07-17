52d5b231de51dbe37659ee3924f0bf743f82d464
csDan31 <csDan31@gmail.com>
2022-07-17 14:59:51 -0400

    added prompt to set board btn and clear grid on reset

--- script.js
+++ script.js
@@ -12,13 +12,14 @@ for(let i = 0; i<256 ; i++) {
     square.style.borderWidth = 'thin';
 } 
 */
-
-function makeBoard(row,col) {
+let grid = 16;
+function makeBoard(grid) {
+    grid = 16;
     let board = document.querySelector('.board');
-    board.style.gridTemplateColumns = "repeat(16, 1fr)";
-    board.style.gridTemplateRows = "repeat(16, 1fr)";
+    board.style.gridTemplateColumns = `repeat(${grid}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${grid}, 1fr)`;
 
-for(let i = 0; i<256 ; i++) {
+for(let i = 0; i<(grid*grid) ; i++) {
     let square = document.createElement('div');
     board.insertAdjacentElement("beforeend",square);
     square.style.border = 'solid';
@@ -27,13 +28,31 @@ for(let i = 0; i<256 ; i++) {
     }}
 
 makeBoard();
-
+// mouseover changes the background to black
 let squares = document.querySelectorAll('.etch');
 
 squares.forEach((square) => {
     square.addEventListener('mouseover', etch);
 })
 function etch(){
-    this.style.backgroundColor = "black";
+    this.style.backgroundColor = 'black';
 }
 
+// set board size
+let boardSize = document.getElementById('board-size');
+
+function enterBoardSize(){
+    let sizeNum = prompt("");
+}
+
+boardSize.addEventListener('click', enterBoardSize);
+
+// reset hover overs
+let resetBtn = document.getElementById('reset');
+
+resetBtn.addEventListener('click', resetGrid);
+
+function resetGrid(){
+    document.querySelectorAll('.etch').forEach((item) => {
+        item.style.backgroundColor = 'white';
+})};
