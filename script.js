65197117fd12d1baca65b4dd2c83d5736a6fbf1d
csDan31 <csDan31@gmail.com>
2022-07-17 23:45:18 -0400

    added set board size and changed border to outline when creating board

--- script.js
+++ script.js
@@ -12,9 +12,10 @@ for(let i = 0; i<256 ; i++) {
     square.style.borderWidth = 'thin';
 } 
 */
+
+
 let grid = 16;
-function makeBoard(grid) {
-    grid = 16;
+function makeBoard() {
     let board = document.querySelector('.board');
     board.style.gridTemplateColumns = `repeat(${grid}, 1fr)`;
     board.style.gridTemplateRows = `repeat(${grid}, 1fr)`;
@@ -22,27 +23,46 @@ function makeBoard(grid) {
 for(let i = 0; i<(grid*grid) ; i++) {
     let square = document.createElement('div');
     board.insertAdjacentElement("beforeend",square);
-    square.style.border = 'solid';
-    square.style.borderWidth = 'thin';
+    square.style.backgroundColor = 'white';
+    square.style.outline = 'solid';
+    square.style.outlineWidth = 'thin';
     square.setAttribute('class','etch');
+
+    let squares = document.querySelectorAll('.etch');
+
+    squares.forEach((square) => {
+    square.addEventListener('mouseover', etch);
+    })
     }}
+
+function etch(){
+    this.style.backgroundColor = 'black';
+}
+    
 
 makeBoard();
 // mouseover changes the background to black
-let squares = document.querySelectorAll('.etch');
+/*let squares = document.querySelectorAll('.etch');
 
 squares.forEach((square) => {
     square.addEventListener('mouseover', etch);
 })
 function etch(){
     this.style.backgroundColor = 'black';
-}
+}*/
 
 // set board size
 let boardSize = document.getElementById('board-size');
 
 function enterBoardSize(){
-    let sizeNum = prompt("");
+    let sizeNum = prompt('How many columns and rows would you like?','');
+    if(sizeNum > 100) {
+        alert("Too big! It might cause it to freeze! Try setting it lower than 100!")
+    } else {
+        resetGrid();
+        grid = sizeNum;
+        makeBoard();
+    }
 }
 
 boardSize.addEventListener('click', enterBoardSize);
@@ -56,3 +76,4 @@ function resetGrid(){
     document.querySelectorAll('.etch').forEach((item) => {
         item.style.backgroundColor = 'white';
 })};
+
